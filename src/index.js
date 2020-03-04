ca64d4319b23a121801995567db931d23a4f57a7
Julia Zaripova <zaripovaur@gmail.com>
2020-03-04 13:21:53 +0400

    add expression-calculator

--- src/index.js
+++ src/index.js
@@ -4,7 +4,25 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+	let countLeft = 0;
+   	let countRight = 0;
+   	let exprArray = expr.split('');
+	for (i of exprArray) {
+		if (i == '(') { 
+			countLeft +=1
+		} else if (i == ')') {
+			countRight +=1
+		}
+	}
+	if (countRight !== countLeft) {throw new Error('ExpressionError: Brackets must be paired')}
+
+	if (expr.includes('/ 0')) {throw new TypeError('TypeError: Division by zero.')}
+
+	let res = new Function('return ' + expr)();
+	if (res == Infinity) {throw new TypeError('TypeError: Division by zero.')}
+
+	return res;
+
 }
 
 module.exports = {
